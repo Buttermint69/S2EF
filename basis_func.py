6a772e319ab294a3cf27028056182867903dd442
Harshit Tiwari <78033216+Buttermint69@users.noreply.github.com>
2022-04-13 02:28:35 +0530

    Update basis_func.py

--- basis_func.py
+++ basis_func.py
@@ -5,12 +5,6 @@ Created on Thu Apr  7 02:27:29 2022
 @author: Harshit
 """
 
-"""
-Copyright (c) Facebook, Inc. and its affiliates.
-
-This source code is licensed under the MIT license found in the
-LICENSE file in the root directory of this source tree.
-"""
 
 import math
 import numpy as np
